57a11e79747061599c22939b9d9a0a25964bb720
ned8800 <tchichikin@mail.ru>
2026-07-05 04:30:22 +0300

    added language options

--- run_after_refresh-plasma.sh
+++ run_after_refresh-plasma.sh
@@ -4,3 +4,7 @@ if command -v plasma-apply-wallpaperimage &> /dev/null; then
     # Укажите точный путь к картинке, которая у вас синхронизируется через dotfiles
     plasma-apply-wallpaperimage "$HOME/Pictures/StarrySur_Mac-3.jpg"
 fi
+
+# Перезапуск подсистемы клавиатуры Plasma
+kquitapp6 kaccess 2>/dev/null; kstart kaccess &
+
